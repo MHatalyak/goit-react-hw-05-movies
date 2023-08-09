ec94aa9fa7fee3f868984ae1047601b0013c4f47
MHatalyak <45709437+MHatalyak@users.noreply.github.com>
2023-08-09 20:02:06 +0000

    Deploying to gh-pages from @ MHatalyak/goit-react-hw-05-movies@12c07c4ef6d63369c6b63aafb5eae89ed3fafaa4 🚀

---
+++ static/js/597.04d5125f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[597],{3597:function(a,e,s){s.r(e);s(2791);var t=s(184);e.default=function(a){var e=a.cast;return(0,t.jsx)("div",{className:"row",children:e.map((function(a){return(0,t.jsxs)("div",{className:"col-md-3 mb-3",children:[(0,t.jsx)("img",{src:"https://image.tmdb.org/t/p/w200/".concat(a.profile_path),alt:a.name,className:"img-fluid"}),(0,t.jsx)("p",{className:"mt-2",children:a.name})]},a.id)}))})}}}]);
+//# sourceMappingURL=597.04d5125f.chunk.js.map
