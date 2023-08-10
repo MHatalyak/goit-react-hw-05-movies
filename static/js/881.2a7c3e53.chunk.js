5b17f2d1412f9935d62600b162d2bafbe7b47738
MHatalyak <45709437+MHatalyak@users.noreply.github.com>
2023-08-10 20:15:30 +0000

    Deploying to gh-pages from @ MHatalyak/goit-react-hw-05-movies@46ec91526b97a030b514cd5bbb959d5af9a2c480 🚀

--- static/js/881.2a7c3e53.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[881],{3881:function(e,i,r){r.r(i);r(2791);var n=r(184);i.default=function(e){var i=e.reviews;return(0,n.jsx)("div",{children:i.length>0?(0,n.jsx)("ul",{children:i.map((function(e){return(0,n.jsxs)("li",{children:[(0,n.jsxs)("p",{children:["Author: ",e.author]}),(0,n.jsx)("p",{children:e.content})]},e.id)}))}):(0,n.jsx)("p",{children:"No reviews available."})})}}}]);
-//# sourceMappingURL=881.2a7c3e53.chunk.js.map
